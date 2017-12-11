fb62184b238b56c41798f740bb4aa13d88e51ab6
annamstarr <31353295+annamstarr@users.noreply.github.com>
2017-12-11 10:01:16 -0800

    yaaaa

--- IntegerList.java
+++ IntegerList.java
@@ -1,33 +1,64 @@
+// ****************************************************************
+// IntegerList.java
+//
+// Define an IntegerList class with methods to create, fill,
+// sort, and search in a list of integers.
+//          
+// ****************************************************************
+import java.util.Scanner;
 
-/**
- * Write a description of class IntegerList here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
-public class IntegerList
-{
-    // instance variables - replace the example below with your own
-    private int x;
+public class IntegerList{
+    int[] list; //values in the list
+    int n = -1;
+    int[] sorted;
 
-    /**
-     * Constructor for objects of class IntegerList
-     */
-    public IntegerList()
-    {
-        // initialise instance variables
-        x = 0;
+    Scanner scan = new Scanner(System.in);
+
+    //create a list of the given size
+    public IntegerList(int size) {
+        list = new int[size];
+        sorted = new int[size];
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    //fill array with integers between 1 and 100, inclusive
+    public void randomize() {
+        for (int i=0; i<list.length; i++) {
+            list[i] = (int)(Math.random() * 100) + 1;
+        }
     }
+
+    //print array elements with indices
+    public void print() {
+        for (int i=0; i<list.length; i++) {
+            System.out.println(i + ":\t" + list[i]);
+        }
+    }
+
+    //return the index of the first occurrence of target in the list.
+    //return -1 if target does not appear in the list
+    public int search(int target) {
+        for (int i = 0; i < list.length ; i++) {
+            if (list[i] == target) {
+                 n = i;
+            }
+        }
+        return n;
+    }
+
+    //sort the list into ascending order using the selection sort algorithm
+    public void selectionSort() {
+        int minIndex;
+        minIndex = list[0];
+
+        for (int i=0; i < list.length-1; i++) {
+            //find smallest element in list starting at location i
+            //swap list[i] with smallest element
+            if (list[i +1] < minIndex) {
+                 minIndex = list[i+1];
+            }
+            sorted[i] = minIndex;
+            minIndex = 0;
+        }
+    }
+
 }
